773404f8af4cfa6c3436326a64094aa79f4d6957
Oscar <oscar.zajac99@gmail.com>
2023-03-05 12:00:26 +0100

    initial commit

---
+++ src/test/java/com/postIT/postIT/PostItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postIT.postIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
